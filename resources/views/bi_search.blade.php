fa2c48205c4c58f3a389840fb29f3c0e9e925d30
José Quinanga <jose.quinanga@muzoltek.com>
2022-05-01 19:17:10 +0100

    Upload dos novos file Transportadora

--- resources/views/bi_search.blade.php
+++ resources/views/bi_search.blade.php
@@ -1,5 +1,10 @@
 @extends ('layouts.visitor') <!--Call of template welcome-->
 @section('content')  <!--Sectino to show content to yield -->
-
-
+<div class="banner-box">
+    <div class="container">
+            <h1 class="header-title padding-top">
+            Descontos vitalícios? Apenas com o Genius!
+            </h1>
+    </div>
+</div>
 @endsection
--- resources/views/layouts/visitor.blade.php
+++ resources/views/layouts/visitor.blade.php
@@ -13,7 +13,6 @@
    
     <link rel="stylesheet" href="{{ asset('assets/css/splide.min.css') }}">
 	<link rel="stylesheet" href="{{ asset('assets/css/magnific-popup.css') }}">
-	<link rel="stylesheet" href="{{ asset('assets/css/select2.min.css') }}">
 	<link rel="stylesheet" href="{{ asset('assets/css/main.css') }}">
 
     <link href="{{ url('assets/css/all.min.css') }}" rel="stylesheet">
@@ -25,10 +24,12 @@
 	<meta name="keywords" content="Viagens Alugar Carro Interprovincial Reservar Viagens Comprar Bilhetes Passageiro Lugares">
 	<meta name="author" content="Mmli Soluções">
     <!-- Scripts -->
-	<script src="{{ url('assets/js/app.js') }}">
-	</script><script src="{{ url('assets/js/jquery.min.js') }}" ></script>
-	<script src="{{ url('assets/js/popper.min.js') }}" ></script>
-    <style>
+    <script src="{{ url('assets/js/jquery.min.js') }}" ></script>
+    <script src="{{ asset('assets/js/bootstrap.bundle.min.js') }}"></script>
+    <script src="{{ asset('assets/js/jquery.easing.min.js') }}"></script>
+    <script src="{{ asset('assets/js/popper.min.js') }}"></script>
+    <script src="{{ url('assets/js/bootstrap.min.js') }}"></script>
+	<style>
    
     </style>
 </head>
@@ -38,21 +39,26 @@
         <nav class="navbar navbar-expand static-top p-1">
 <div class="container">
 <div class="col-4">
-<a class="navbar-brand " href="">
-    <img src="{{ url('assets/img/logo/logo.png') }}"  alt="logo" class="logo img-responsive">
+<a class="navbar-brand " href="{{ route('index') }}">
+    <img src="{{ url('assets/img/logo/logo.png') }}"  alt="logo" class="logotipo img-responsive">
     </a>
 </div>
-<div class="col-4"></div>
+<div class="col-1"></div>
+<div class="col-3">
+<ul class="list-group list-group-horizontal timeTable text-right">
+  <li class="list-group-items"><i class="fa fa-calendar"></i> Segunda - Sexta <i class="fa fa-clock"></i> 8:00 Am - 18:00 Pm</li>
+</ul>
+</div>
 <div class="col-4">
-<div class="header__actions">
+<div class="header__action">
     <div class="header__action">
-								<a class="header-btn" href="cars.html">
-									Registar
+								<a class="btn btn-outline-primary" href="cars.html">
+								 <i class="fa fa-user"></i>	Registar
 								</a>
 							</div>
                             <div class="header__action">
-								<a class="header-btn" href="cars.html">
-									Iniciar Sessão
+								<a class="btn btn-outline-primary" href="cars.html">
+                                <i class="fa fa-lock"></i> Iniciar Sessão
 								</a>
 							</div>
 
@@ -62,45 +68,45 @@
 </div>
   </nav>
 	<header class="header">
-		<div class="container">
-			<div class="row">
-				<div class="col-12">
-					<div class="header__content">
+        <div class="container">
 
-						<div class="header__menu">
-							<ul class="header__nav">
-                                <li class="header__nav-item">
-									<a href="{{ route('home') }}" class="header__nav-link">Página Inicial</a>
-								</li>
-                                <li class="header__nav-item">
-                                    <a href="#" class="header__nav-link">Bilhetes</a>
-								</li>
+        <nav class="navbar navbar-expand-lg">
+<!--  <a class="navbar-brand" href="#">Página inicial</a>-->
+  <button class="navbar-toggler btn-light" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon text-primary"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link" href="{{ route('index') }}"><i class="fa fa-home"></i> Página inicial <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+      <a class="nav-link" href="#"><i class="fa fa-credit-card-alt"></i> Bilhetes e Horários</a>
+      </li>
+      <li class="nav-item">
+      <a class="nav-link" href="#"><i class="fa fa-map-marker"></i> Pontos de Venda e Embarque</a>
+      </li>
+      <li class="nav-item">
+      <a class="nav-link" href="#"><i class="fa fa-car"></i> Alugueres de Carros</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#"><i class="fa fa-list"></i> Planos e Preçários</a>
+      </li>
+      
+      <li class="nav-item">
+        <a class="nav-link" href="#"><i class="fa fa-phone"></i> Contactos</a>
+      </li>
+    </ul>
+  </div>
+</nav>
 
-
-								<li class="header__nav-item">
-                                    <a href="#" class="header__nav-link">Como Funciona</a>
-								</li>
-								<li class="header__nav-item">
-									<a href="#" class="header__nav-link">Centro de Ajuda</a>
-								</li>
-
-
-							</ul>
-						</div>
-
-					
-			</div>
 		</div>
+        
 	</header>
-    
-	<!-- end header -->
+    <!-- end header -->
     <div id='app'>
     <main>
-    <div class="bg-img">
-        <div class="bg-img-dark">
          @yield('content')
-        </div>
-        </div>
     </main>
     </div>
     
